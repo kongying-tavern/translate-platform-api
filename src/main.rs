57913b83d4525139b766714ff08a669e2b523914
nan-mu <mu.nan.11@outlook.com>
2024-05-08 00:07:43 +0800

    feat: socket连接框架

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+mod error;
+use error::Result;
+
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("log begin"); // TODO: 之后需要换成log库
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    let mut net_tasks = Vec::new();
+    loop {
+        let (stream, _) = listener.accept().await?;
+        net_tasks.push(tokio::spawn(async move {
+            let _handle = stream;
+        }));
+    }
+}
