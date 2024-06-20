989306d864ff2f373a01c52d414807be9f6e9bed
nan-mu <mu.nan.11@outlook.com>
2024-06-20 17:46:49 +0800

    chore: 删除了之前socket的实现，该分支下是actix_web和test_tokio_postgres的两个官方测例，已经通过

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # translate-platform-api
 Kongying Translate Platform API.
 
+## 任务线
+
+
 ## CKANGELOG
 
 应该使用git-cliff生成：
--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,55 @@
-mod database;
-mod error;
-// mod user;
-use error::Result;
-// use user::register; 完了，忘记之前自己写的时候怎么想的了
-use tokio::{
-    io::AsyncWriteExt,
-    net::{self, TcpListener},
-};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-use database::{create, write};
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    println!("log begin"); // TODO: 之后需要换成log库
-
-    // 初始化socket
-    let listener = TcpListener::bind("127.0.0.1:6379").await?;
-    let mut net_tasks = Vec::new();
-
-    // 初始化数据库
-    let postgresql = database::build("main").await;
-    let client = database::connect(&postgresql).await.unwrap();
-
-    // 初始化表，目前只有用户表，后面有PDManger再说
-    create::create_user_table(&client).await.unwrap();
-    let mut database_init_count = 0;
-
-    loop {
-        //这层循环捕捉链接会话，理论上会在链接到来之前准备好数据库对象
-        let client = match database::connect(&postgresql).await {
-            Ok(client) => client,
-            Err(e) => {
-                eprintln!("数据库连接失败: {:?}", e);
-                database_init_count += 1;
-                if database_init_count <= 10 {
-                    continue;
-                } else {
-                    panic!("数据库连接失败次数过多，程序退出")
-                }
-            }
-        };
-        let (mut stream, _) = listener.accept().await?;
-
-        net_tasks.push(tokio::spawn(async move {
-            let command = to_command(&mut stream).await;
-            loop {
-                match &command {
-                    Ok(Command::Register(_, _)) => {
-                        write::insert_user(&client, command.unwrap()).await.unwrap();
-                        return ();
-                    }
-                    Ok(Command::Ping) => {
-                        stream.write_all(b"pong\r\n").await.unwrap();
-                    }
-                    Err(_) => unimplemented!("将错误抛给客户端"),
-                }
-            }
-            //这层循环保持和一个客户端会话的收发信
-        }));
-    }
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
-async fn to_command(_stream: &mut net::TcpStream) -> Result<Command> {
-    unimplemented!("解析客户端发来的数据，解析为具体的命令")
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
 
-pub enum Command {
-    /// Ping，用于测试连接
-    Ping,
-    /// email, password
-    Register(String, String),
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[actix_web::test]
+async fn test_tokio_postgres() {
+    use actix_web::rt;
+    use tokio_postgres::NoTls;
+    // TODO：之后将密码设置为读取内置文件填写，开发阶段就先这样吧
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=postgres password=dev_password", NoTls)
+            .await
+            .unwrap();
+
+    rt::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    // Now we can execute a simple statement that just returns its parameter.
+    let rows = client
+        .query("SELECT $1::TEXT", &[&"hello world"])
+        .await
+        .unwrap();
+
+    // And then check that we got back the same string we sent over.
+    let value: &str = rows[0].get(0);
+    assert_eq!(value, "hello world");
 }
