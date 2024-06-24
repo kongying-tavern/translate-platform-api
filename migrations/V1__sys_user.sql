a8a10bc434acefd95c348f73f35d56234cfe72e1
nan-mu <mu.nan.11@outlook.com>
2024-06-24 17:20:27 +0800

    fix(database): 同步PD导出表格代码

--- migrations/V1__sys_user.sql
+++ migrations/V1__sys_user.sql
@@ -1,14 +1,12 @@
-
-/* --------------- 创建表 --------------- */
--- DROP TABLE IF EXISTS sys_user;
+DROP TABLE IF EXISTS sys_user;
 CREATE TABLE sys_user(
     version INTEGER NOT NULL DEFAULT  1,
-    create_by VARCHAR,
+    creator_id BIGINT NOT NULL DEFAULT  0,
     create_time TIMESTAMP,
-    update_by VARCHAR,
+    updater_id BIGINT NOT NULL DEFAULT  0,
     update_time TIMESTAMP,
     del_flag BOOLEAN NOT NULL DEFAULT  false,
-    id VARCHAR NOT NULL,
+    id SERIAL NOT NULL,
     username VARCHAR(32) NOT NULL DEFAULT  '',
     password VARCHAR(255) NOT NULL DEFAULT  '',
     role INTEGER NOT NULL DEFAULT  -1,
@@ -16,11 +14,12 @@ CREATE TABLE sys_user(
     locale VARCHAR(32) NOT NULL DEFAULT  '',
     PRIMARY KEY (id)
 );
+
 COMMENT ON TABLE sys_user IS '用户';
 COMMENT ON COLUMN sys_user.version IS '乐观锁';
-COMMENT ON COLUMN sys_user.create_by IS '创建人';
+COMMENT ON COLUMN sys_user.creator_id IS '创建人';
 COMMENT ON COLUMN sys_user.create_time IS '创建时间';
-COMMENT ON COLUMN sys_user.update_by IS '更新人';
+COMMENT ON COLUMN sys_user.updater_id IS '更新人';
 COMMENT ON COLUMN sys_user.update_time IS '更新时间';
 COMMENT ON COLUMN sys_user.del_flag IS '是否删除';
 COMMENT ON COLUMN sys_user.id IS 'ID';
