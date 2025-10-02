d3d967dcf1dffd5c36e308a7cca7a734c51868ed
nan mu <mu.nan.11@outlook.com>
2025-10-03 01:38:50 +0800

    ✨ 入口函数
    几乎全抄https://github.com/kongying-tavern/genshin-cloud-rust/blob/master/packages/router/src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use axum::serve;
+use std::env;
+use tokio::net::TcpListener;
 use tracing_subscriber::util::SubscriberInitExt;
 use translate_platform_api::run;
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt()
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
@@ -9,8 +12,31 @@ async fn main() -> anyhow::Result<()> {
         .finish()
         .init();
 
-    // 调用库中的run函数
-    run().await?;
+    let postgres_db = env::var("POSTGRES_DB").expect("POSTGRES_DB is not set");
+    let postgres_user = env::var("POSTGRES_USER").expect("POSTGRES_USER is not set");
+    let postgres_password = env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD is not set");
+    let postgres_host = env::var("POSTGRES_HOST").unwrap_or_else(|_| "localhost".to_string());
+    let postgres_port = env::var("POSTGRES_PORT").unwrap_or_else(|_| "5432".to_string());
+    let postgres_url = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        postgres_user, postgres_password, postgres_host, postgres_port, postgres_db
+    );
+
+    let app_port = env::var("APP_PORT").unwrap_or_else(|_| "3000".to_string());
+
+    tracing::info!(
+        postgres_url = %postgres_url,
+        app_port = %app_port,
+        "已加载环境变量"
+    );
+
+    let router = router()
+        .await?
+        .into_make_service_with_connect_info::<SocketAddr>();
+
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", app_port)).await?;
+
+    serve(listener, router).await?;
 
     println!("Goodbye!");
     Ok(())
